34440885b3905bc52eb32f28350eabe2189f0435
Anaihita123 <103839944+Anaihita123@users.noreply.github.com>
2022-04-23 20:10:27 +0530

    Add files via upload

---
+++ Basic Data Types Activity/Session 3 - JavaScript Tutor- Working With Basic Data Types (code).js
@@ -0,0 +1,24 @@
+let greetings = "Welcome to my house";
+let name = "Anaihita";
+let title = "Anaihita";
+let worldName = "Anaihita";
+
+let age = 11;
+let year = 2022;
+let century = 2100;
+
+let songUsed = ['Harry Styles', 'Olivia Rodrigo', 'Billie Eilish'];
+
+let hasFavSong = true;
+
+
+console.log(greetings + " my Name is " + name + ".");
+console.log("I'm the " + title);
+console.log("As the member of " + worldName + ", I like to play the guitar.");
+console.log("It is an instrument");
+console.log("For example, I started playing when I was " + age + " years old.");
+console.log("In my world, you are in year" + year + ", the " + century + "th century." + "\n");
+console.log("Does anyone have any questions?");
+console.log("Me: Do you have a favourite singer?");
+console.log("Human: " + hasFavSong + ", my favourite singer is " + songUsed[0] + "." + "\n");
+console.log("You are free to do anything you want!");
